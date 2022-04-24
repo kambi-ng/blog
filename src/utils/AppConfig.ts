6c76e7b4bda69866c50275e245f7c90d446c13c3
Dennis Al Baihaqi Walangadi <53461759+dnswd@users.noreply.github.com>
2022-04-24 22:58:08 +0700

    chore(meta): change config domain

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -2,7 +2,7 @@ export const AppConfig = {
   site_name: 'Kambing',
   title: 'Kambing',
   description: 'Blog Kambing',
-  url: 'https://kambing.net',
+  url: 'https://kambi.ng',
   locale: 'en',
   author: 'Kambing Org',
   pagination_size: 6,

bacee7fb28c878e3daa25dcc9d0dbd9ae6bc59a2
Ghibranalj <24712554+Ghibranalj@users.noreply.github.com>
2022-06-29 13:33:51 +0200

    [fix] markdown code without language wont crash

--- src/utils/Markdown.ts
+++ src/utils/Markdown.ts
@@ -4,6 +4,9 @@ import { marked } from 'marked';
 export function markdownToHtml(markdown: string) {
   marked.setOptions({
     highlight: (code, lang) => {
+      if (!lang) {
+        return hljs.highlightAuto(code).value;
+      }
       return hljs.highlight(lang, code).value;
     },
   });
